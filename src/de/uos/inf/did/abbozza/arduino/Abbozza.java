33449fef2a61c117d1bd6843b169017fe5b81306
mbrinkmeier <mbrinkmeier@uni-osnabrueck.de>
2018-02-16 08:57:02 +0100

    * update check added * version changed to 0.12.0.0

--- src/de/uos/inf/did/abbozza/arduino/Abbozza.java
+++ src/de/uos/inf/did/abbozza/arduino/Abbozza.java
@@ -15,7 +15,6 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 /**
  * @fileoverview The main class for the abbozza! server
  * @author michael.brinkmeier@uni-osnabrueck.de (Michael Brinkmeier)
@@ -40,14 +39,19 @@ import de.uos.inf.did.abbozza.handler.SerialHandler;
 import de.uos.inf.did.abbozza.plugin.PluginConfigPanel;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
@@ -60,12 +64,12 @@ import processing.app.packages.UserLibrary;
 public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
 
     public static final int SYS_MAJOR = 0;
-    public static final int SYS_MINOR= 11;
+    public static final int SYS_MINOR = 11;
     public static final int SYS_REV = 0;
     public static final int SYS_HOTFIX = 0;
     public static final String SYS_REMARK = "(arduino)";
     public static final String SYS_VERSION = SYS_MAJOR + "." + SYS_MINOR + "." + SYS_REV + "." + SYS_HOTFIX + " " + SYS_REMARK;
-    
+
     public static Color COLOR = new Color(91, 103, 165);
     private static int counter;
 
@@ -76,11 +80,11 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
     private Editor editor;
 
     public static URI jarUri;
-    
+
     public static boolean isGlobal;
     public static boolean localExists;
     public static String name;
-    
+
     // During class load check is this calss is loaded from the global tool jar
     static {
         isGlobal = true;
@@ -90,15 +94,15 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
             // Get jar file
             jarUri = Abbozza.class.getProtectionDomain().getCodeSource().getLocation().toURI();
             System.out.println("abbozza!: Initializing " + jarUri.getPath());
-            
+
             // Get 
             File sketchbookFolder = BaseNoGui.getSketchbookFolder();
             System.out.println("abbozza!: Checking sketchbook folder: " + sketchbookFolder.toURI().toString());
-            File localJar = new File(sketchbookFolder,"/tools/Abbozza/tool/abbozza-arduino.jar");
-            if ( localJar.exists() ) {
+            File localJar = new File(sketchbookFolder, "/tools/Abbozza/tool/abbozza-arduino.jar");
+            if (localJar.exists()) {
                 System.out.println("abbozza!: LOCAL jar found at " + localJar.toURI().toString());
                 localExists = true;
-                if ( localJar.toURI().toString().startsWith(jarUri.toString())) {
+                if (localJar.toURI().toString().startsWith(jarUri.toString())) {
                     isGlobal = false;
                 } else {
                     isGlobal = true;
@@ -111,46 +115,43 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
         } catch (URISyntaxException ex) {
         }
 
-        if ( jarUri == null ) {
+        if (jarUri == null) {
             System.out.println("abbozza! in panic! Cannot find abbozza-arduino.jar!");
             System.exit(1);
-        }        
+        }
     }
-    
-    
-    
+
     @Override
-    public void init(Editor editor) {        
-        
-        if ( isGlobal && localExists ) {
+    public void init(Editor editor) {
+
+        if (isGlobal && localExists) {
             System.out.println("Suppressing global abbozza! tool, since local version exists!");
             return;
         }
-        
+
         String version = BaseNoGui.VERSION_NAME;
         int pos = version.indexOf('.');
         arduino_major = Integer.parseInt(version.substring(0, pos));
         int pos2 = version.indexOf('.', pos + 1);
         arduino_minor = Integer.parseInt(version.substring(pos + 1, pos2));
         arduino_rev = Integer.parseInt(version.substring(pos2 + 1));
-        AbbozzaLogger.out("Found arduino version " + arduino_major + "." +arduino_minor + "." + arduino_rev,AbbozzaLogger.INFO);
-        
+        AbbozzaLogger.out("Found arduino version " + arduino_major + "." + arduino_minor + "." + arduino_rev, AbbozzaLogger.INFO);
+
         this.editor = editor;
-        
+
         super.init("arduino");
-        
-        if ( config.startAutomatically() ) {
+
+        if (config.startAutomatically()) {
             startServer();
-            if ( config.startBrowser() ) {
+            if (config.startBrowser()) {
                 startBrowser("arduino.html");
             }
         }
     }
 
-    
     @Override
     public void run() {
-        if ( isGlobal && localExists ) {
+        if (isGlobal && localExists) {
             System.out.println("Suppressing global abbozza! tool, since local version exists!");
             return;
         }
@@ -158,7 +159,7 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
         // Do not start a second Abbozza instance!
         if (Abbozza.getInstance() != this) {
             return;
-        }        
+        }
 
         startServer();
         startBrowser("arduino.html");
@@ -167,21 +168,20 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
     @Override
     public void setPaths() {
         super.setPaths();
-        
+
         sketchbookPath = PreferencesData.get("sketchbook.path");
-        if ( sketchbookPath.contains("%HOME%")) {
+        if (sketchbookPath.contains("%HOME%")) {
             sketchbookPath = sketchbookPath.replace("%HOME%", System.getProperty("user.home"));
         }
         // configPath = sketchbookPath + "/tools/Abbozza/Abbozza.cfg";
         localJarPath = sketchbookPath + "/tools/Abbozza/tool/";
         globalJarPath = PreferencesData.get("runtime.ide.path") + "/";
         abbozzaPath = globalJarPath;
-        
+
         localPluginPath = sketchbookPath + "/tools/Abbozza/plugins";
         globalPluginPath = globalJarPath + "/tools/Abbozza/plugins";
     }
- 
-    
+
     @Override
     public void registerSystemHandlers() {
         httpServer.createContext("/abbozza/board", new BoardHandler(this, false));
@@ -189,11 +189,9 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
         httpServer.createContext("/abbozza/serial", new SerialHandler(this));
     }
 
-    
     public void serialMonitor() {
         this.editor.handleSerial();
     }
-
 
     public void findJarsAndDirs(JarDirHandler jarHandler) {
         jarHandler.clear();
@@ -207,8 +205,7 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
     public void adaptConfigDialog(AbbozzaConfigDialog dialog) {
         dialog.addPanel(new PluginConfigPanel());
     }
-    
-    
+
     public void print(String message) {
         AbbozzaLogger.out(message);
     }
@@ -220,18 +217,17 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
 
     @Override
     public String getMenuTitle() {
-        if ( isGlobal && localExists ) {
+        if (isGlobal && localExists) {
             System.out.println("Suppressing global abbozza! tool, since local version exists!");
             return "abbozza! (deactivated)";
-        }        
+        }
         return "abbozza!";
     }
 
     public Editor getEditor() {
         return editor;
     }
-    
-    
+
     // Moves the arduino IDE window to the back
     @Override
     public void toolToBack() {
@@ -263,25 +259,24 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
         }
     }
 
-    
     @Override
     public int compileCode(String code) {
-        
+
         compileMsg = "";
         compileErrorMsg = "";
-        
+
         String result = null;
-        
-        toolSetCode(code);        
+
+        toolSetCode(code);
 
         // Redirect error stream
         PrintStream origErr = System.err;
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         PrintStream newErr = new PrintStream(buffer);
         System.setErr(newErr);
-            
+
         // Compile sketch                
-        try {            
+        try {
             AbbozzaLogger.out(AbbozzaLocale.entry("msg.compiling"), AbbozzaLogger.INFO);
             editor.statusNotice("abbozza!: " + AbbozzaLocale.entry("msg.compiling"));
             result = editor.getSketchController().build(false, false);
@@ -293,16 +288,15 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
             editor.statusNotice("abbozza!: " + AbbozzaLocale.entry("msg.error_compiling"));
             AbbozzaLogger.out(AbbozzaLocale.entry("msg.error_compiling"), AbbozzaLogger.INFO);
         }
-        
 
         // Reset error stream
-        newErr.flush();            
+        newErr.flush();
         System.setErr(origErr);
-  
+
         // Fetch response
         compileErrorMsg = buffer.toString();
 
-        if ( result != null ) {
+        if (result != null) {
             return 0;
         } else {
             return 1;
@@ -320,24 +314,24 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
         group.enumerate(threads, false);
 
         monitorHandler.suspend();
-        
-        toolSetCode(code);        
+
+        toolSetCode(code);
 
         // Redirect error stream
         PrintStream origErr = System.err;
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         PrintStream newErr = new PrintStream(buffer);
         System.setErr(newErr);
-        
+
         try {
-            AbbozzaLogger.out(AbbozzaLocale.entry("msg.compiling"),AbbozzaLogger.INFO);
+            AbbozzaLogger.out(AbbozzaLocale.entry("msg.compiling"), AbbozzaLogger.INFO);
             editor.handleExport(false);
-            AbbozzaLogger.out(AbbozzaLocale.entry("msg.done_compiling"),AbbozzaLogger.INFO);
+            AbbozzaLogger.out(AbbozzaLocale.entry("msg.done_compiling"), AbbozzaLogger.INFO);
         } catch (Exception e) {
             editor.statusError(e);
             AbbozzaLogger.out(AbbozzaLocale.entry("msg.error_compiling"), AbbozzaLogger.INFO);
         }
-    
+
         Thread[] threads2 = new Thread[group.activeCount()];
         group.enumerate(threads2, false);
 
@@ -358,41 +352,44 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
             }
             i--;
         }
-        
+
         // Wait for the termination of the export thread
-        AbbozzaLogger.out("Waiting for upload",AbbozzaLogger.INFO);
+        AbbozzaLogger.out("Waiting for upload", AbbozzaLogger.INFO);
         while ((last != null) && (last.isAlive())) {
         }
-        AbbozzaLogger.out("Upload ended",AbbozzaLogger.INFO);
-        
+        AbbozzaLogger.out("Upload ended", AbbozzaLogger.INFO);
+
         PreferencesData.setBoolean("editor.save_on_verify", flag);
 
         // Reset error stream
-        newErr.flush();            
+        newErr.flush();
         System.setErr(origErr);
-  
+
         // Fetch response
         compileErrorMsg = buffer.toString();
-        AbbozzaLogger.out(compileErrorMsg,AbbozzaLogger.INFO);
-                
-        if ( compileErrorMsg.contains("error") ) {
+        AbbozzaLogger.out(compileErrorMsg, AbbozzaLogger.INFO);
+
+        if (compileErrorMsg.contains("error")) {
             return 1;
         }
         return 0;
-        
+
     }
 
     public boolean checkLibrary(String name) {
         UserLibrary lib = Base.getLibraries().getByName(name);
-        if ( lib != null ) return true;
+        if (lib != null) {
+            return true;
+        }
         return false;
     }
-    
+
     public String getSerialPort() {
         BoardPort port = Base.getDiscoveryManager().find(PreferencesData.get("serial.port"));
-        if ( port != null )
+        if (port != null) {
             return port.getAddress();
-        
+        }
+
         return null;
     }
 
@@ -409,17 +406,19 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
     public File queryPathToBoard(String path) {
         return new File(path);
     }
-    
+
     public String getSystemVersion() {
         return SYS_VERSION;
-    };
+    }
+
+    ;
 
     @Override
     public boolean installPluginFile(InputStream stream, String name) {
         File target = new File(sketchbookPath + "/libraries/Abbozza/" + name);
         try {
             AbbozzaLogger.info("Copying " + name + " to " + target.toString());
-            Files.copy(stream, target.toPath(), StandardCopyOption.REPLACE_EXISTING );
+            Files.copy(stream, target.toPath(), StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException ex) {
             AbbozzaLogger.err("Could not copy " + name + " to " + target.toString());
             return false;
@@ -427,6 +426,42 @@ public class Abbozza extends AbbozzaServer implements Tool, HttpHandler {
         return true;
     }
 
+    @Override
+    public void installUpdate(String version, String updateUrl) {
+        try {
+            // 1st step: Rename current jar
+            // Find current jar
+            URL curUrl = AbbozzaServer.class.getProtectionDomain().getCodeSource().getLocation();
+            File cur = new File(curUrl.toURI());
+            AbbozzaLogger.out("Current jar found at " + cur.getAbsolutePath(), AbbozzaLogger.INFO);
+            SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+            String today = format.format(new Date());
+            File dir = new File(cur.getParentFile().getAbsolutePath());
+            if (!dir.exists()) {
+                AbbozzaLogger.out("Creating directory " + dir.getPath(), AbbozzaLogger.INFO);
+                dir.mkdir();
+            }
+            AbbozzaLogger.out("Moving old version to " + dir.getPath() + "/Abbozza." + today + ".jar", AbbozzaLogger.INFO);
+            cur.renameTo(new File(dir.getPath() + "/Abbozza." + today + ".jar"));
+            
+            // 2nd step: Download new version
+            AbbozzaLogger.out("Downloading version " + version, AbbozzaLogger.INFO);
+            URL url = new URL(updateUrl + "abbozza-arduino.jar");
+            URLConnection conn = url.openConnection();
+            byte buffer[] = new byte[4096];
+            int n = -1;
+            InputStream ir = conn.getInputStream();
+            FileOutputStream ow = new FileOutputStream(new File(curUrl.toURI()));
+            while ((n = ir.read(buffer)) != -1) {
+                ow.write(buffer, 0, n);
+            }
+            ow.close();
+            ir.close();
+            AbbozzaLogger.out("Stopping Arduino IDE", AbbozzaLogger.INFO);
+            System.exit(0);
+        } catch (Exception ex) {
+            Logger.getLogger(AbbozzaServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }
-
